c14f5db577ab2a6dbccfba9d6a53e96fe211e764
tthamil <79051723+tthamil@users.noreply.github.com>
2026-05-11 13:04:51 +0800

    Add files via upload

--- Sign.jsx
+++ Sign.jsx
@@ -2,7 +2,33 @@
 
 function Sign() {
     return (
-        <div><h1>Sign in Page</h1></div>
+        <div className="title">
+            <div className="container">
+                <h1>New Sign Page</h1>
+            </div>
+            <div className="class-sign">
+                <label htmlFor="email">Email Address</label>
+                <input type="text" placeholder="Email address" />
+            </div>
+            <div className="class-sign">
+                <label htmlFor="password">Password</label>
+                <input type="text" placeholder="Password" />
+            </div>
+            <div className="class-sign">
+                <label htmlFor="Confirm">Confirm Password</label>
+                <input type="text" placeholder="Confirm Password" />
+            </div>
+            <div className="btnclass">
+                <button type="submit">
+                    Sign In
+                </button>
+            </div>
+            <p>Click here for Login
+                <button className="btn-class">Login</button>
+            </p>
+
+
+        </div>
     )
 }
 export default Sign;
